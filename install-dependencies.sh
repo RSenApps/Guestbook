9caaf3a2ef5426e73ab45b9b04455990a5847a81
Ryan Senanayake <RSenApps@gmail.com>
2014-12-28 15:41:52 -0800

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,7 +9,7 @@ if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
   echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t platform-tools &&
-  echo y | android update sdk -u -a -t build-tools-21.1.2 &&
+  echo y | android update sdk -u -a -t build-tools-21.0.0 &&
   echo y | android update sdk -u -a -t extra-android-support &&
   echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
   touch $DEPS
