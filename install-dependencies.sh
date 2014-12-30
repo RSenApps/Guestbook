68808cb746f67b8b73e40b6a3c624c9e678ee4ca
Ryan Senanayake <rsenapps@gmail.com>
2014-12-29 16:52:09 -0800

    try to install everything

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,18 +17,15 @@ android list sdk -e
 if [ ! -e $DEPS ]; then
 
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-
-  echo y | android update sdk -u -a -t platform-tools &&
-  echo y | android update sdk -u -a -t tools &&
-  echo y | android update sdk -u -a -t android-21 &&
-  echo y | android update sdk -u -a -t build-tools-21.0.0 &&
-  echo y | android update sdk -u -a -t extra-google-google_play_services &&
-  echo y | android update sdk -u -a -t extra-android-m2repository &&
-  echo y | android update sdk -u -a -t extra-android-support &&
-  echo y | android update sdk -u -a -t extra-google-m2repository &&
-  echo y | android update sdk -u -a -t extra-google-play_licensing &&
-  echo y | android update sdk -u -a -t sys-img-x86-android-21 &&
-  echo y | android update sdk -u -a -t addon-google_apis-google-21 &&
+  echo y | android update sdk -u -a &&
+ # echo y | android update sdk -u -a -t platform-tools &&
+  #echo y | android update sdk -u -a -t tools &&
+  #echo y | android update sdk -u -a -t android-21 &&
+  #echo y | android update sdk -u -a -t build-tools-21.0.0 &&
+  #echo y | android update sdk -u -a -t extra-google-google_play_services &&
+  #echo y | android update sdk -u -a -t extra-android-m2repository &&
+  #echo y | android update sdk -u -a -t extra-android-support &&
+  #echo y | android update sdk -u -a -t extra-google-m2repository &&
 
   # DO NOT CREATE EMULATOR FOR THE MOMENT
   # echo n | android create avd -n testing -f -t android-21 &&
